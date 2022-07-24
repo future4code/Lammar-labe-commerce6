bc9d02ba46b7269908a3711c87865a9245040061
Glhazyanno Linhares Braga <glbraga9@gmail.com>
2022-07-24 18:10:40 -0300

    testea

---
+++ src/Components/App/style.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const ListContainer = styled.div`
+   display: flex;
+   justify-content: space-between;
+   flex-wrap:wrap;
+   border: 1px solid black;
+   width: 700px;
+   flex-wrap:wrap;
+`
+export const AppContainer = styled.div`
+   display: flex;
+
+   flex-wrap:wrap;
+
+`
+export const CarrinhoContainer = styled.div`
+  display: flex;
+  flex-wrap:wrap;
+  width: 300px;
+  border: 1px solid black;
+
+`
